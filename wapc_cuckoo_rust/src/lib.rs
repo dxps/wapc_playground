1c19ef9f82506007defcf5051ba513847a60b06d
dxps <marius.ileana@gmail.com>
2022-04-20 19:12:14 +0300

    chore(wapc_cuckoo_rust): lib.rs warns removed, cargo deps updates

--- wapc_cuckoo_rust/src/lib.rs
+++ wapc_cuckoo_rust/src/lib.rs
@@ -4,7 +4,6 @@ extern crate cuckoofilter;
 
 mod generated;
 
-//extern crate wasm_bindgen;
 use cuckoofilter::CuckooFilter;
 use std::collections::hash_map::DefaultHasher;
 extern crate wapc_guest as guest;
@@ -13,19 +12,14 @@ pub use generated::*;
 use guest::prelude::*;
 
 lazy_static! {
-    static ref cf: CuckooFilter<DefaultHasher> = {
-        let CF: CuckooFilter<DefaultHasher> = load_data();
-        CF
-    };
+    static ref CF: CuckooFilter<DefaultHasher> = load_data();
 }
+
 fn load_data() -> CuckooFilter<DefaultHasher> {
     let words = vec!["foo", "bar", "xylophone", "milagro"];
     let mut new_cf: CuckooFilter<DefaultHasher> = CuckooFilter::new();
-    // let mut insertions = 0;
     for s in &words {
-        if new_cf.test_and_add(s) {
-            // insertions += 1;
-        }
+        new_cf.test_and_add(s).unwrap();
     }
     new_cf
 }
@@ -35,8 +29,9 @@ pub fn wapc_init() {
     Handlers::register_check_word_exists(check_word_exists);
     Handlers::register_handle_input(handle_input)
 }
+
 fn check_word_exists(member: String) -> HandlerResult<String> {
-    let exists = cf.contains(&member);
+    let exists = CF.contains(&member);
     println!("{}", exists);
     Ok(exists.to_string())
 }
